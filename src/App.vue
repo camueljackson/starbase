247f435c2420eb8e9dc3f686b7d5a9b098b5e310
camueljackson <camille.a.pelletier@gmail.com>
2019-07-04 04:21:47 -0400

    fixed cors issue by adding Allow CORS extension; first API call

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
-  <div id="app">{{title}}</div>
+  <div id="app">
+    {{title}}
+    <button class="btn btn-primary" @click="fetchCharacter">Test fetch</button>
+  </div>
 </template>
 
 <script>
@@ -9,6 +12,15 @@ export default {
     return {
       title: "Generate your team"
     };
+  },
+  methods: {
+    fetchCharacter() {
+      fetch("https://swapi.co/api/people/", {
+        method: "GET"
+      })
+        .then(response => response.json())
+        .then(json => console.log(json));
+    }
   }
 };
 </script>
